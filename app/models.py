153697077126adfa8dfe5fab979eba2fc275c688
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:07:24 +0800

    Rename password field to password_hash and add set_password() and check_password() methods

--- app/models.py
+++ app/models.py
@@ -7,7 +7,11 @@ class User(UserMixin,db.Model):
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100),unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
 
 
-
+    def set_password(self, password):
+            self.password_hash = generate_password_hash(password)
+            
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
