8b2a18452e146a3af5cddddb263565e397caa942
DharunSomalingam <dharun2004.s@gmail.com>
2025-04-30 22:11:08 +0800

    Modified the Location of config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database.db'  # Example for SQLite
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional, but recommended to disable
