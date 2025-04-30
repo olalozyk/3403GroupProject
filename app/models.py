99fa002a987a58cfe6e05e3587450fcf89db9060
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 22:52:17 +0800

    Ensure User model uses 'id' as primary key for Flask-Login compatibility

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100),unique=True, nullable=False)
