2d47455cadffaecf0ec1115b4b9db35a0c5d0fe8
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:03:17 +0800

    Inherit from UserMixin to support session management with Flask-Login

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from app import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
