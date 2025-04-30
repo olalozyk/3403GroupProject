3ae204c361b8df7af2a0e6dc59ed8784fa2928ec
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:13:35 +0800

    Add Flask-Login to project setup

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
