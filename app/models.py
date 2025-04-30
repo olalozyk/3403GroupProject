578c9514bc738cbde9a70aa386f698dda51347e3
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:00:51 +0800

    Rename Users model to User to follow Flask naming convention

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 
-class Users(db.Model):
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
