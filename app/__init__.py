2d475abeae4b640f0992271ad645c826647782b6
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:15:49 +0800

    Set LoginManager login view to 'login' route

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' #redirects to login page if user is not logged in
+
 from app import routes,models  # Import routes here
