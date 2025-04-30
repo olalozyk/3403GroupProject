8c6eed09e9254057d5ccc54cf6f89f8a523ba6d4
DharunSomalingam <dharun2004.s@gmail.com>
2025-04-30 22:07:42 +0800

    Created Config.py with Configure SQLAlchemy with SQLite and disable track modifications

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database.db'  # Example for SQLite
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional, but recommended to disable
