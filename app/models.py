5917340a3cc499800917b80f87ecb28a85ec8c1e
Aleksandra Lozyk <olalozyk@icloud.com>
2025-04-30 23:08:38 +0800

    Add repr method for better logging

--- app/models.py
+++ app/models.py
@@ -16,3 +16,6 @@ class User(UserMixin,db.Model):
             
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+            return f'<User {self.email}>'
