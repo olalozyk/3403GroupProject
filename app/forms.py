32c61443a63ac3a4083e1a995b3030466efc853e
weishen98 <litshen98@gmail.com>
2025-05-02 23:50:21 +0800

    Update 'Users' to 'User'

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+    remember_me = BooleanField("Remember Me")
+
 
 class RegistrationForm(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    contact = StringField("Contact")
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[
         DataRequired(), EqualTo("password", message="Passwords must match.")
     ])
     submit = SubmitField("Register")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email is already registered. Please use a different email address.')
