9b8eb70bfbb2e6e44ef9e5965cd0f84b959b9645
DharunSomalingam <dharun2004.s@gmail.com>
2025-07-28 14:04:26 +0800

    Fix: encode DB password in connection URI

--- microblog.py
+++ microblog.py
@@ -1,3 +1,7 @@
+# ✅ Must be first
+import eventlet
+eventlet.monkey_patch()
+
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -6,17 +10,11 @@ from flask_migrate import Migrate
 from app import socketio, create_app, db
 from app.config import ProductionConfig  # instead of DeploymentConfig
 
-
 # Make these available globally for Flask CLI
 application = create_app(ProductionConfig)
 migrate = Migrate(application, db)
 
 # Run with SocketIO if script is executed directly
 if __name__ == '__main__':
-    import eventlet
-    import eventlet.wsgi
-    eventlet.monkey_patch()
-
+    # No need to monkey_patch again here
     socketio.run(application, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
